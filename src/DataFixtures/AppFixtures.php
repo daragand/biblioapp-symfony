5267a79ab111961f14f7538e24a4f5199ae3b8e1
Demba Thiam <daragan.dooms@gmail.com>
2023-09-25 16:01:24 +0200

    ✨ ajout de l'entité Category ✨ execution des fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use App\Entity\Book;
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -13,6 +14,34 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create($fakerLocale='fr_FR');
 
+        $categories =[
+            'Roman',
+            'Bande dessinée',
+            'poésie',
+            'Fantastique',
+            'Documentaire',
+            'Policier',
+            'Aventure',
+            'Biographie'
+
+        ];
+
+            $objectCategory=[];
+
+
+        
+
+            for ($i=0; $i < count($categories); $i++) {
+                $category = new Category();
+                $category->setName($categories[$i])
+                    ->setDescription($faker->sentence(1))
+                    ->setImage($faker->image())
+                    ;
+
+                    array_push($objectCategory,$category);
+                    $manager->persist($category);
+                    
+            }
         for ($i=0; $i < 500; $i++) { 
             $book = new Book();
         $book->setTitle($faker->sentence(1))
@@ -21,6 +50,7 @@ class AppFixtures extends Fixture
             ->setPages(203)
             ->setYear($faker->numberBetween(1950,2023))
             ->setIsAvailable($faker->boolean())
+            ->setCategory($objectCategory[rand(0,7)])
             ;
         $manager->persist($book);
         
---
+++ src/Entity/Category.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
+    #[ORM\OneToMany(mappedBy: 'category', targetEntity: Book::class)]
+    private Collection $books;
+
+    public function __construct()
+    {
+        $this->books = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Book>
+     */
+    public function getBooks(): Collection
+    {
+        return $this->books;
+    }
+
+    public function addBook(Book $book): static
+    {
+        if (!$this->books->contains($book)) {
+            $this->books->add($book);
+            $book->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBook(Book $book): static
+    {
+        if ($this->books->removeElement($book)) {
+            // set the owning side to null (unless already changed)
+            if ($book->getCategory() === $this) {
+                $book->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+}
