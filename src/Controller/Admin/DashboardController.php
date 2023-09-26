a561a577806b6a9ce4fe357cb5d1f332f49ecb71
Demba Thiam <daragan.dooms@gmail.com>
2023-09-26 15:41:37 +0200

    ⚡️ installation de easyadmin 4, configuration, make:admin:crud

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Book;
+use App\Entity\Author;
+use App\Entity\Editor;
+use App\Entity\Format;
+use App\Entity\Category;
+use App\Entity\Client;
+use App\Entity\Language;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+  
+
+        
+       
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        return $this->render('admin/dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Biblioapp');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::section('Books');
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Bibliothèque', 'fas fa-book', Book::class);
+        yield MenuItem::linkToCrud('Client', 'fas fa-users', Client::class);
+        yield MenuItem::section('Autorités');
+        yield MenuItem::linkToCrud('Auteurs', 'fas fa-pen-nib', Author::class);
+        yield MenuItem::linkToCrud('Editeurs', 'fas fa-building', Editor::class);
+        yield MenuItem::section('Paramètres');
+        yield MenuItem::linkToCrud('Catégories', 'fas fa-list', Category::class);
+        yield MenuItem::linkToCrud('Formats', 'fas fa-arrows-up-down-left-right', Format::class);
+        yield MenuItem::linkToCrud('Langues', 'fas fa-language', Language::class);
+        yield MenuItem::section('Autres');
+        yield MenuItem::linkToRoute('Retour au site', 'arrow-left', 'app_page');
+    }
+}
