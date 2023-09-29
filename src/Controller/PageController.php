4ca37b4aa9366697184562c0df1b98516eb6fc2f
Demba Thiam <daragan.dooms@gmail.com>
2023-09-29 17:00:17 +0200

    ✨ récupération du formulaire contact et envoi mail

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -5,7 +5,10 @@ namespace App\Controller;
 use App\Form\ContactType;
 use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
@@ -25,6 +28,8 @@ class PageController extends AbstractController
     #[Route('/contact', name: 'app_contact', methods:['GET','POST'])]
     public function contact(
         ContactType $form,
+        Request $request, 
+        MailerInterface $mailer
         ): Response
     {
         //on récupère la classe de l'élément à savoir le contact
@@ -32,26 +37,38 @@ class PageController extends AbstractController
 
 
 //on réceptionne les données du formulaire avec Request
-
+        $form->handleRequest($request);
 
 // si le formulaire est soumis et valide alors ...
+if ($form->isSubmitted() && $form->isValid()) {
+    # code...
+    
+    // on récupère les données du formulaire pour les placer dans le mail
+    $name = $form->get('name')->getData();
+    $email = $form->get('email')->getData();
+    $subject = $form->get('sujet')->getData();
+    $message = $form->get('message')->getData();
 
-
-// on récupère les données du formulaire pour les placer dans le mail
-
-
-// on instancie un nouveau mail 
-
-
-// On paramètre le mail 
-
-
-// On envoie le mail
-
-
-// On informe l'utilisateur
-
-
+    // dd($name,$email,$subject,$messsage);
+    
+    // on instancie un nouveau mail 
+    
+    // On paramètre le mail 
+    
+    $mail = (new Email())
+        ->from($email)
+        ->to('contact@biblioapp.fr')
+        ->priority(Email::PRIORITY_HIGH)
+        ->subject($subject)
+        ->html('<div>Vous avez reçu le message suivant de ' . $name . '.<br> Contenu : <br>'. $message. '</div>'
+);
+    // On envoie le mail
+    $mailer->send($mail);
+    
+    // On informe l'utilisateur
+    
+    $this->addFlash('success', 'Votre message a bien été envoyé');
+}
 
 
 
